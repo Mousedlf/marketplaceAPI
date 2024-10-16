4fbfe268197c8a28e7052666159200e95109268a
CARROTThomas <97225091+CARROTThomas@users.noreply.github.com>
2024-10-16 10:00:21 +0200

    add template mail

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -31,4 +31,25 @@ class MailerController extends AbstractController
         $mailer->send($email);
         return $this->redirectToRoute('app_login');
     }
+
+    #[Route('/template/key')]
+    public function displayTemplateOrder(): Response
+    {
+        return $this->render('mailer/layout/key.html.twig', [
+            'mail' => "exemple@mail.com",
+            'date_order' => "16/10/2024",
+            'product_name' => "Clé API Fruits",
+            'offre_name' => "10000 R.",
+            'product_price' => 20,
+            'activation_key' => "12345-6789-0435"
+        ]);
+    }
+
+    #[Route('/template/user')]
+    public function accountDeletion(): Response
+    {
+        return $this->render('mailer/layout/user.html.twig', [
+            'mail' => "exemple@mail.com",
+        ]);
+    }
 }
