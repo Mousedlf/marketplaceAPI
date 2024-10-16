4effee01fdedc93e8ee487f6b1b9a44662221e19
mouse <dlfcaroline@gmail.com>
2024-10-16 11:01:02 +0200

    added SHOW details of api page / new migration with one added prop to entity API / "app_cart_add_item" of CartController needed for template to work

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\API;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class APIController extends AbstractController
 {
-    #[Route('/a/p/i', name: 'app_a_p_i')]
-    public function index(): Response
+    #[Route('/api/{id}', name: 'show_api')]
+    public function show(API $API): Response
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'APIController',
+        return $this->render('api/show.html.twig', [
+            'API' => $API,
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\APIRepository;
+use App\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,7 +16,7 @@ class HomeController extends AbstractController
         $apis = $APIRepository->findAll();
 
         return $this->render('home/index.html.twig', [
-            'apis' => $apis,
+            'apis' => $apis
         ]);
     }
 }
