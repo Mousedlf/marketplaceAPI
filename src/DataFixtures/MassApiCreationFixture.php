0fa7a3d7bf6efad53ad865f448bf80753c0e6e2f
Thibaut <thibautstachnick@gmail.com>
2024-10-16 11:17:50 +0200

    add a way to use fixtures in order to get apis and order for testing purpose

--- src/DataFixtures/MassApiCreationFixture.php
+++ src/DataFixtures/MassApiCreationFixture.php
@@ -2,15 +2,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\API;
+use App\Entity\Order;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class MassApiCreationFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $apiRepository = $manager->getRepository(API::class);
+        $faker = Factory::create();
+        $user = new User();
+        $user->setCreatedAt(new \DateTime());
+        $user->setEmail($faker->email());
+        $user->setPassword($faker->password());
+        $manager->persist($user);
+        $manager->flush();
+
+        for ($i = 0; $i < 20; $i++) {
+            $api = new API();
+            $api->setCreatedBy($user);
+            $api->setName($faker->name);
+            $api->setDescription($faker->text);
+            $manager->persist($api);
+        }
+        $manager->flush();
+
+
+        for ($i = 0; $i < 10; $i++) {
+            $order = new Order();
+            $order->setTotal($faker->randomFloat());
+            $order->setByUser($user);
+            $order->addAPI($apiRepository->getRandomApi());
+            $manager->persist($order);
+        }
 
         $manager->flush();
     }
