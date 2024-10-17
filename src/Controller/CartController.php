880dc29094d6577eacaf805ee1cc3418c1d38a99
CARROTThomas <97225091+CARROTThomas@users.noreply.github.com>
2024-10-17 16:49:15 +0200

    add template cart + function

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,6 +6,8 @@ use App\Entity\API;
 use App\Entity\Offer;
 use App\Service\CartService;
 use Stripe\Checkout\Session;
+use Stripe\Price;
+use Stripe\Product;
 use Stripe\Stripe;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -56,18 +58,37 @@ class CartController extends AbstractController
     }
 
 
-    #[Route('/pay', name: 'app_pay')]
-    public function pay(): Response
+    #[Route('/items', name: 'app_cart_items')]
+    public function displayCart(
+        CartService $cartService,
+    ): Response
     {
-        Stripe::setApiKey('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
+        return $this->render('cart/index.html.twig', [
+            "cart" => $cartService->getCart(),
+            "total" => $cartService->getTotal(),
+        ]);
+    }
+
+
+    #[Route('/pay', name: 'app_pay')]
+    public function pay(
+        CartService $cartService,
+    ): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY']);
+
+        $total = $cartService->getTotal() * 100;
 
         $session = Session::create([
             'payment_method_types' => ['card'],
             'line_items' => [[
                 'price_data' => [
-                    'currency' => 'usd',
+                    'currency' => 'eur',
                     'product_data' => [
-                        'name' => 'T-shirt',
+                        'name' => 'Key API',
                     ],
                     'unit_amount' => 2000,
                 ],
@@ -75,7 +96,7 @@ class CartController extends AbstractController
             ]],
             'mode' => 'payment',
             'success_url' => 'http://localhost:8000/success?session_id={CHECKOUT_SESSION_ID}',
-            'cancel_url' => 'http://localhost:8000/cancel',
+            'cancel_url' => 'http://localhost:8000/cart/items',
         ]);
 
         return $this->redirect($session->url, 303);
